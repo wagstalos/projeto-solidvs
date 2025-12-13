8dafe0fd0c6955703d4a77ade0e4f67dfe5f7462
Wagner Paulo <wagstalos@hotmail.com>
2025-12-13 16:33:34 -0300

    feat: added swiper

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,7 +1,7 @@
 AOS.init();
 
 document.addEventListener("DOMContentLoaded", function () {
-  const swiper = new Swiper(".swiper", {
+  const swiper = new Swiper(".s-feedback .swiper", {
     speed: 400,
     spaceBetween: 100,
     pagination: {
@@ -12,5 +12,23 @@ document.addEventListener("DOMContentLoaded", function () {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
     },
+  });
+
+  const swiperService = new Swiper(".swiper-service", {
+    speed: 400,
+    spaceBetween: 30,
+    slidesPerView: 1,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      768: {
+          slidesPerView: 2,
+      },
+      1200: {
+          slidesPerView: 3,
+      }
+    }
   });
 });
