4fce457a3cb110fd6b087d7faedd1104af0ba7c4
Wagner Paulo <wagstalos@hotmail.com>
2025-12-13 16:48:26 -0300

    feat: added btn next prev sliders

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -22,6 +22,10 @@ document.addEventListener("DOMContentLoaded", function () {
       el: ".swiper-pagination",
       clickable: true,
     },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
     breakpoints: {
       768: {
           slidesPerView: 2,
